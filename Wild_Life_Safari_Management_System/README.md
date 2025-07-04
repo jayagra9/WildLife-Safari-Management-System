4a97b00d0efb6bd0758e0a33c77282a8188bc4f7
Jayagra Sawsarani <163627058+jayagra9@users.noreply.github.com>
2025-07-04 09:51:50 +0530

    Add files via upload

---
+++ Wild_Life_Safari_Management_System/README.md
@@ -0,0 +1,2 @@
+# Wild_Life_Safari_Management_System
+Web Based System To manage Wild Life Safari Company
---
+++ Wild_Life_Safari_Management_System/about.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+
+    <!-- swiper css link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="./css/style.css">
+
+</head>
+
+<body>
+
+<body>
+
+<!-- header section starts -->
+<section class="header">
+
+<a href="home.php" class="logo"><span>S</span>afari <span>L</span>anka</a>
+
+    <nav class="navbar">
+        <a href="about.php">About Us</a>
+        <a href="./contact_us/index.php">Contact Us</a>
+        <a href="package.php">Our Packages</a>
+        <a href="./reservations/add.php">Reservations</a>
+        <a href="./payment/add_payment.php">Check Out</a>
+        <a href="donation/addDonation.php">Donate Now!</a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<div class="heading" style="background:url(images/aboutus6.jpg) no-repeat">
+    <h1>about us</h1>
+</div>
+
+<!-- about section starts -->
+
+<section class="about">
+    <div class="image">
+        <img src="images/aboutone.jpg" alt="a photo of a family">
+    </div>
+
+    <div class="content">
+        <h3>why choose us?</h3>
+        <p>Our team comprises knowledgeable guides and wildlife enthusiasts who possess extensive expertise in Sri Lanka's diverse ecosystems and wildlife habitats. We specialize in curating personalized safari itineraries that cater to your interests, whether you seek encounters with majestic elephants, elusive leopards, or colorful birdlife.<br>
+        Moreover, Safari Lanka is dedicated to responsible tourism. We prioritize wildlife conservation and sustainable practices, ensuring that our adventures have a positive impact on local communities and the environment. When you choose Safari Lanka, you not only embark on a thrilling safari but also contribute to the preservation of Sri Lanka's natural heritage.</p>
+        <div class="icons-container">
+            <div class="icons">
+                <i class="fas fa-map"></i>
+                <span>top destinations</span>
+            </div>
+            <div class="icons">
+                <i class="fas fa-hand-holding-usd"></i>
+                <span>affordable price</span>
+            </div>
+            <div class="icons">
+                <i class="fas fa-headset"></i>
+                <span>24/7 guide service</span>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- about section ends -->
+
+
+
+
+
+
+
+</body>
